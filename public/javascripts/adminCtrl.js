bba82c1195e8beae390962869b7688e769ac2740
Ralph Ryan Bautista <Ralph Ryan Bautista>
2017-01-18 20:42:24 +0800

    node mailer

--- public/javascripts/adminCtrl.js
+++ public/javascripts/adminCtrl.js
@@ -3,14 +3,14 @@ var app = angular.module('mainApp',[]);
 var testMike;
 function adminCtrl($scope, $http) {
     $scope.vm = this;
-    this.testMike = "hey hey";
-     $http.get('graph.facebook.com/')
+    this.testMike = "hey Marius";
+    /* $http.get('graph.facebook.com/')
                 .success(function(data) {
                         $scope.todos = data;
                 })
                 .error(function(data) {
                         console.log('Error: ' + data);
-                });
+                });*/
 }
 
 //Register controller with module  
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 /* Admin Page */
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,12 +10,42 @@ router.get('/', function(req, res, next) {
 
 /* Create guest list page. */
 router.get('/guestlist', function(req, res, next) {
-  res.render('index', { title: 'The Guest List' });
+  res.render('guestlist', { title: 'The Guest List page ko!' });
 });
 
 /* Create wedding details page. */
 router.get('/weddinginfo', function(req, res, next) {
   res.render('index', { title: 'Wedding Information' });
+});
+
+router.get('/sendmail', function(req, res, next) {
+  var smtpConfig = {
+     host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: "ralphryan.bautista@gmail.com",
+        pass: "gmail316061984"
+      }
+  };
+   var transporter = nodemailer.createTransport(smtpConfig);
+   // replace hardcoded options with data passed (somedata)
+  var mailOptions = {
+    from: 'ralphryan.bautista@gmail.com', // sender address
+    to: 'dirk3162003@yahoo.com', // list of receivers
+    subject: 'Test email', // Subject line
+    text: 'this is some text', //, // plaintext body
+    html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+  }
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      return false;
+    }else{
+      console.log('Message sent: ' + info.response);
+      return true;
+    };
+  });
 });
 
 
